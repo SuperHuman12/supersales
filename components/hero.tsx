e81982091c0d81ba0659f4ccab0aa7ddebe20485
Vaibhav <qvmail83@gmail.com>
2024-03-13 17:12:30 +0000

    trynow

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,6 @@
 import VideoThumb from '@/public/images/hero-image.png'
 import ModalVideo from '@/components/modal-video'
+import Image from 'next/image' // Import the Image component from Next.js
 
 export default function Hero() {
   return (
@@ -42,6 +43,26 @@ export default function Hero() {
                   <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">View Demos</a>
                 </div>
               </div>
+
+              <div className="mx-auto mt-4 mb-1 max-w-xs md:max-w-lg flex flex-col items-center justify-center space-y-4 lg:mt-12">
+                  <div className="flex justify-center overflow-hidden">
+                  <img alt="Ana" src="https://d33wubrfki0l68.cloudfront.net/679ef4276d68b8417b1e0e9410ba8b0d81fa2145/1ba87/assets/images/influencers/ana.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Anthony" src="https://d33wubrfki0l68.cloudfront.net/154f3c5cfb6472e3c3271b06e99d94fc5508bcc5/16c0e/assets/images/influencers/anthony.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Ayush" src="https://d33wubrfki0l68.cloudfront.net/c6f92e1ae2769f7cc13d3ece99c16d591564bf4a/7114a/assets/images/influencers/ayush.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Damon" src="https://d33wubrfki0l68.cloudfront.net/bab5fdfe723835e12924bb22730b06ede3dd6192/8e44b/assets/images/influencers/damon.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Roberto" src="https://d33wubrfki0l68.cloudfront.net/fa50407454a0301048b246364e635b2a6c58045c/b2181/assets/images/influencers/roberto.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Alex MacCaw" src="https://d33wubrfki0l68.cloudfront.net/75af78aa9bf70a1c124768d0b60e37a5cd1bd9b7/11cb5/assets/images/influencers/alex.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Tibo" src="https://d33wubrfki0l68.cloudfront.net/a55f9800ba70015a07c09f87c30a24f4fcfdbb83/4c349/assets/images/influencers/tibo.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+    <img alt="Corey Haines" src="https://d33wubrfki0l68.cloudfront.net/36ee27ca00ba110131c925e463c42c885e523d10/de5f0/assets/images/influencers/corey.png" className="rounded-full ring-2 ring-gray-100" style={{width: '2.5rem', height: '2.5rem', marginLeft: '-0.65rem', position: 'relative', border: '3px solid #191919'}} />
+                  </div>
+
+                  <p className="mt-2 pb-2 text-center text-base text-gray-900 md:mt-0 md:ml-4">
+                      <span className="block">Join <strong className="font-semibold mx-2">100+ of creators, and brands</strong></span>
+                  </p>
+              </div>
+
+
+
             </div>
           </div>
 
