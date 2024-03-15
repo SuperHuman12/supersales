d5d60006dff980c7edd0fa6d30845065e104b71b
Vaibhav <qvmail83@gmail.com>
2024-03-15 19:00:14 +0000

    trynow

--- app/(auth)/integration/page.tsx
+++ app/(auth)/integration/page.tsx
@@ -114,7 +114,7 @@ export default function ResetPassword() {
           <p>Embed interactive product demos in your documentation and turn all users into power users.</p>
         </div>
         <div className="flex space-x-2 items-center" >
-          <a href="https://app.gitbook.com/integrations/arcade" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
+          <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
             <span>Install</span>
             {/* SVG or icon for arrow here if needed */}
           </a>
@@ -128,13 +128,12 @@ export default function ResetPassword() {
 
       {/* Integration Overview */}
       <div>
-        <h3 className="h3 font-bold mt-4">Overview</h3>
-        <p>Arcade lets you create interactive product demos in minutes. Turn all users into power users.</p>
-        <p>This integration allows you to embed Arcade interactive demos into your GitBook documentation.</p>
-        <h3 className="h3 font-bold mt-4">How it works</h3>
-        <p>Embedding an Arcade flow in your documentation is simple - just paste an Arcade flow link.</p>
-        <h3 className="h3 font-bold mt-4">Configure</h3>
-        <p>To install on a single space navigate to integrations in sub-navigation.</p>
+        <h3 className="h3 font-bold mt-4 mb-4">Overview</h3>
+        <p>Arcade lets you create interactive product demos in minutes and turn all users into power users. Let users experience your product through interactive, guided demos. No more pausing and rewinding, users can progress at their own pace. This integration allow you to embed Arcade interactive demos into your GitBook documentation.</p>
+        <h3 className="h3 font-bold mt-4 mb-4">How it works</h3>
+        <p>Embedding an Arcade flow in your documentation is simple - just paste an Arcade flow link into the GitBook editor. The app will turn the link into an Arcade embed showing your recorded demo.</p>
+        <h3 className="h3 font-bold mt-4 mb-4">Configure</h3>
+        <p>To install on a single space navigate to integrations in sub-navigation. If you prefer to install the Arcade integration on multiple or all spaces, navigate to your organization settings and enable it from the integrations panel. This will give you access to the custom arcade block.</p>
       </div>
 
     </div>
