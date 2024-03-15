c316c1809e470909d27099fa5151dcc2e31687a5
Vaibhav <qvmail83@gmail.com>
2024-03-15 19:13:48 +0000

    trynow

--- app/(auth)/integration/page.tsx
+++ app/(auth)/integration/page.tsx
@@ -24,72 +24,72 @@ export default function ResetPassword() {
             {/* 1st item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
                   <span className="text-dark font-bold">Linear</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
             {/* 2nd item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/Yo5xVE0shfRCQvIwRDGU6KqIbzM.png" alt="" />
                   <span className="text-dark font-bold">Slack</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
             {/* 3rd item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/FS1of0gxF0A6UP7GmCpIBKQM.png" alt="" />
                   <span className="text-dark font-bold">Arcade</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
             {/* 4th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/m9aUcDVUAGUkouzars7JOPL4pzU.png" alt="" />
                   <span className="text-dark font-bold">Discord</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
             {/* 5th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/DA65II8e2dwHkaDz1CqTV7Pra6k.png" alt="" />
                   <span className="text-dark font-bold">Fathom</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
             {/* 6th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
               <div>
-              <div className="flex items-center space-x-2 mb-4">
+                <div className="flex items-center space-x-2 mb-4">
                   <img className="w-10 h-10" src="https://framerusercontent.com/images/Dies87xzsIZvlFyRdxkVgDUnU.png" alt="" />
                   <span className="text-dark font-bold">Figma</span>
-              </div>
-              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
-              <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
+                </div>
+                <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+                <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">Install</button>
               </div>
             </div>
 
@@ -97,46 +97,49 @@ export default function ResetPassword() {
 
 
 
+
+            {/* Integration Details */}
+
           <div className="space-y-8 bg-gray-100 p-8">
 
-          <div className="flex space-y-6" >
-      <div className="space-y-4">
-        <div className="flex items-center space-x-4" >
-          <div className="w-20 h-20 rounded-full overflow-hidden" >
-            <img src="https://framerusercontent.com/images/FS1of0gxF0A6UP7GmCpIBKQM.png" alt="" className="w-full h-full object-contain"/>
+            <div className="flex space-y-6" >
+              <div className="space-y-4">
+                <div className="flex items-center space-x-4" >
+                  <div className="w-20 h-20 rounded-full overflow-hidden" >
+                    <img src="https://framerusercontent.com/images/FS1of0gxF0A6UP7GmCpIBKQM.png" alt="" className="w-full h-full object-contain" />
+                  </div>
+                  <div className="space-y-2" >
+                    <h1 className="text-2xl font-bold text-gray-800">Arcade</h1>
+                    <p className="text-md text-gray-600">By GitBook</p>
+                  </div>
+                </div>
+                <div className="text-gray-800" >
+                  <p>Embed interactive product demos in your documentation and turn all users into power users.</p>
+                </div>
+                <div className="flex space-x-2 items-center" >
+                  <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
+                    <span>Install</span>
+                    {/* SVG or icon for arrow here if needed */}
+                  </a>
+                </div>
+              </div>
+              <div className="relative w-full" >
+                <img src="https://framerusercontent.com/images/JwSD9igebl5YjP1p0XBssmuRU4.png" alt="" className="w-full h-full object-contain" />
+              </div>
+            </div>
+
+
+            {/* Integration Overview */}
+            <div>
+              <h3 className="h3 font-bold mt-4 mb-4">Overview</h3>
+              <p>Arcade lets you create interactive product demos in minutes and turn all users into power users. Let users experience your product through interactive, guided demos. No more pausing and rewinding, users can progress at their own pace. This integration allow you to embed Arcade interactive demos into your GitBook documentation.</p>
+              <h3 className="h3 font-bold mt-4 mb-4">How it works</h3>
+              <p>Embedding an Arcade flow in your documentation is simple - just paste an Arcade flow link into the GitBook editor. The app will turn the link into an Arcade embed showing your recorded demo.</p>
+              <h3 className="h3 font-bold mt-4 mb-4">Configure</h3>
+              <p>To install on a single space navigate to integrations in sub-navigation. If you prefer to install the Arcade integration on multiple or all spaces, navigate to your organization settings and enable it from the integrations panel. This will give you access to the custom arcade block.</p>
+            </div>
+
           </div>
-          <div className="space-y-2" >
-            <h1 className="text-2xl font-bold text-gray-800">Arcade</h1>
-            <p className="text-md text-gray-600">By GitBook</p>
-          </div>
-        </div>
-        <div className="text-gray-800" >
-          <p>Embed interactive product demos in your documentation and turn all users into power users.</p>
-        </div>
-        <div className="flex space-x-2 items-center" >
-          <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
-            <span>Install</span>
-            {/* SVG or icon for arrow here if needed */}
-          </a>
-        </div>
-      </div>
-      <div className="relative w-full" >
-        <img src="https://framerusercontent.com/images/JwSD9igebl5YjP1p0XBssmuRU4.png" alt="" className="w-full h-full object-contain"/>
-      </div>
-    </div>
-
-
-      {/* Integration Overview */}
-      <div>
-        <h3 className="h3 font-bold mt-4 mb-4">Overview</h3>
-        <p>Arcade lets you create interactive product demos in minutes and turn all users into power users. Let users experience your product through interactive, guided demos. No more pausing and rewinding, users can progress at their own pace. This integration allow you to embed Arcade interactive demos into your GitBook documentation.</p>
-        <h3 className="h3 font-bold mt-4 mb-4">How it works</h3>
-        <p>Embedding an Arcade flow in your documentation is simple - just paste an Arcade flow link into the GitBook editor. The app will turn the link into an Arcade embed showing your recorded demo.</p>
-        <h3 className="h3 font-bold mt-4 mb-4">Configure</h3>
-        <p>To install on a single space navigate to integrations in sub-navigation. If you prefer to install the Arcade integration on multiple or all spaces, navigate to your organization settings and enable it from the integrations panel. This will give you access to the custom arcade block.</p>
-      </div>
-
-    </div>
 
 
 
