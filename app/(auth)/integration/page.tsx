96ef4b3cc94beab4d4e56bb501bf707a3e78edaf
Vaibhav <qvmail83@gmail.com>
2024-03-15 17:46:45 +0000

    trynow

--- app/(auth)/integration/page.tsx
+++ app/(auth)/integration/page.tsx
@@ -22,78 +22,81 @@ export default function ResetPassword() {
             </h2>
           </div>
 
-          <div className="space-y-4">
-      <div className="bg-gray-100 p-4 rounded-lg shadow">
-        <div className="space-y-2">
-          {/* Integration Item 1 */}
-          <a href="./integrations/linear" className="block">
-            <div className="relative rounded overflow-hidden">
-              <img className="w-full h-full object-cover" src="https://framerusercontent.com/images/TpVgEjlOmmrPt10GnAZxILWqLvU.svg" alt="" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <div className="flex items-center space-x-2">
+
+
+          {/* Items */}
+          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
                   <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
                   <span className="text-white font-bold">Linear</span>
-                </div>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
               </div>
             </div>
-          </a>
-          {/* Integration Item 2 */}
-          <a href="./integrations/slack" className="block">
-            <div className="relative rounded overflow-hidden">
-              <img className="w-full h-full object-cover" src="https://framerusercontent.com/images/TpVgEjlOmmrPt10GnAZxILWqLvU.svg" alt="" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <div className="flex items-center space-x-2">
-                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/Yo5xVE0shfRCQvIwRDGU6KqIbzM.png" alt="" />
-                  <span className="text-white font-bold">Slack</span>
-                </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
+                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
+                  <span className="text-white font-bold">Linear</span>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
               </div>
             </div>
-          </a>
-          {/* Integration Item 3 */}
-          <a href="./integrations/arcade" className="block">
-            <div className="relative rounded overflow-hidden">
-              <img className="w-full h-full object-cover" src="https://framerusercontent.com/images/TpVgEjlOmmrPt10GnAZxILWqLvU.svg" alt="" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <div className="flex items-center space-x-2">
-                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/FS1of0gxF0A6UP7GmCpIBKQM.png" alt="" />
-                  <span className="text-white font-bold">Arcade</span>
-                </div>
+
+            {/* 3rd item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
+                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
+                  <span className="text-white font-bold">Linear</span>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
               </div>
             </div>
-          </a>
-          {/* Integration Item 4 */}
-          <a href="./integrations/discord" className="block">
-            <div className="relative rounded overflow-hidden">
-              <img className="w-full h-full object-cover" src="https://framerusercontent.com/images/TpVgEjlOmmrPt10GnAZxILWqLvU.svg" alt="" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <div className="flex items-center space-x-2">
-                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/m9aUcDVUAGUkouzars7JOPL4pzU.png" alt="" />
-                  <span className="text-white font-bold">Discord</span>
-                </div>
+
+            {/* 4th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
+                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
+                  <span className="text-white font-bold">Linear</span>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
               </div>
             </div>
-          </a>
-          {/* Integration Item 5 */}
-          <a href="./integrations/figma" className="block">
-            <div className="relative rounded overflow-hidden">
-              <img className="w-full h-full object-cover" src="https://framerusercontent.com/images/TpVgEjlOmmrPt10GnAZxILWqLvU.svg" alt="" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
-              <div className="absolute bottom-0 left-0 p-4">
-                <div className="flex items-center space-x-2">
-                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/Dies87xzsIZvlFyRdxkVgDUnU.png" alt="" />
-                  <span className="text-white font-bold">Figma</span>
-                </div>
+
+            {/* 5th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
+                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
+                  <span className="text-white font-bold">Linear</span>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
               </div>
             </div>
-          </a>
-          {/* Add more items as needed */}
-        </div>
-      </div>
-    </div>
+
+            {/* 6th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+              <div>
+              <div className="flex items-center space-x-2">
+                  <img className="w-10 h-10 rounded-full" src="https://framerusercontent.com/images/l4nnn6kmtzSkPgFoWWlwzYFzEA.png" alt="" />
+                  <span className="text-white font-bold">Linear</span>
+              </div>
+              <p className="text-gray-600 text-center">Attract your audience by creating a blog.</p>
+              </div>
+            </div>
+
+          </div>
+
+
+
 
 
         </div>
