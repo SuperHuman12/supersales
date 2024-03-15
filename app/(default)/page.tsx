cb307a9d02ac063521679b65ce372ca12f1a90c8
Vaibhav <qvmail83@gmail.com>
2024-03-15 14:22:58 +0000

    trynow

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,7 +8,6 @@ import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Showcases from '@/components/showcase'
-import Templates from '@/components/templates'
 import Newsletter from '@/components/newsletter'
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
       <Hero />
       <FeaturesBlocks />
       <Features />
-      <Templates />
       <Showcases />
       <Testimonials />
       <Newsletter />
