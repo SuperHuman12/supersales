7f6a994a1a97e619ad8a06e5b323644ac039766f
Vaibhav <qvmail83@gmail.com>
2024-03-16 17:57:15 +0000

    trynow

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
               </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="app.notionbear.com">Get free lifetime plan</a>
+                  <a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a>
                 </div>
                 <div>
                   <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">View Example</a>
