bd6d84cc7bd11065782d1c7c77841ee62156a934
Vaibhav <qvmail83@gmail.com>
2024-03-16 18:41:53 +0000

    trynow

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -8,175 +8,175 @@ export default function FeaturesBlocks() {
         <div>
 
 
-        <section className="py-8 mb-8">
+          <section className="py-8 mb-8">
             <div className="flex">
-                <div className="w-36 md:w-48 mt-6">
-                    <img
-                        alt="image"
-                        loading="lazy"
-                        width="132"
-                        height="53.855999999999995"
-                        decoding="async"
-                        className="w-full h-auto"
-                        src="https://assets.super.so/186f4256-f467-4f79-8d6f-40defe12b9b4/images/c56e53c9-812d-4afc-b28b-c82bc3d50992/notion.svg"
-                    />
-                </div>
+              <div className="w-36 md:w-48 mt-6">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  width="132"
+                  height="53.855999999999995"
+                  decoding="async"
+                  className="w-full h-auto"
+                  src="https://assets.super.so/186f4256-f467-4f79-8d6f-40defe12b9b4/images/c56e53c9-812d-4afc-b28b-c82bc3d50992/notion.svg"
+                />
+              </div>
             </div>
             <div className="text-start mt-6">
-                <h2 className="h2 font-bold mb-4">Your content lives in Notion</h2>
-                <p className="text-gray-700">Your work stays in a place you control and love, while Super handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.</p>
+              <h2 className="h2 font-bold mb-4">Your content lives in Notion</h2>
+              <p className="text-gray-700">Your work stays in a place you control and love, while Super handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.</p>
             </div>
             <div className="flex justify-start mt-6 w-full">
-                <div>
-                    <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d41acd65-b5b1-4482-8444-5d811bf9bbd1/notion/w=4416,quality=90"
-                    />
-                </div>
+              <div>
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-full"
+                  src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d41acd65-b5b1-4482-8444-5d811bf9bbd1/notion/w=4416,quality=90"
+                />
+              </div>
             </div>
-        </section>
+          </section>
 
 
-        <section className="py-8 mb-8">
+          <section className="py-8 mb-8">
             <div className="flex">
-                <div className="w-36 md:w-48 mt-6">
-                    <img
-                        alt="image"
-                        loading="lazy"
-                        width="132"
-                        height="53.855999999999995"
-                        decoding="async"
-                        className="w-full h-auto"
-                        src="https://assets.super.so/186f4256-f467-4f79-8d6f-40defe12b9b4/images/c56e53c9-812d-4afc-b28b-c82bc3d50992/notion.svg"
-                    />
-                </div>
+              <div className="w-36 md:w-48 mt-6">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  width="132"
+                  height="53.855999999999995"
+                  decoding="async"
+                  className="w-full h-auto"
+                  src="https://assets.super.so/186f4256-f467-4f79-8d6f-40defe12b9b4/images/c56e53c9-812d-4afc-b28b-c82bc3d50992/notion.svg"
+                />
+              </div>
             </div>
             <div className="text-start mt-6">
-                <h2 className="h2 font-bold mb-4">Customize to match your brand</h2>
-                <p className="text-gray-700">Your work stays in a place you control and love, while Super handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.</p>
+              <h2 className="h2 font-bold mb-4">Customize to match your brand</h2>
+              <p className="text-gray-700">Your work stays in a place you control and love, while Super handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.</p>
             </div>
             <div className="flex justify-start mt-6 w-full">
+              <div>
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-full"
+                  src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d41acd65-b5b1-4482-8444-5d811bf9bbd1/notion/w=4416,quality=90"
+                />
+              </div>
+            </div>
+          </section>
+
+
+
+          <section className="mt-2">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center py-12 md:pb-20">
+              <h2 className="h2 mb-4">Really Easy To Use Website Builder</h2>
+              <p className="text-xl text-gray-600">Notion Bear is a powerful website builder for startups, solo-entrepreneurs and hackers. Try it for free.</p>
+            </div>
+
+
+
+            {/* Items */}
+            <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+
+              {/* 1st item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/aipowered.png"
+                />
                 <div>
-                    <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d41acd65-b5b1-4482-8444-5d811bf9bbd1/notion/w=4416,quality=90"
-                    />
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">AI-Powered</h4>
+                  <p className="text-gray-600 text-center">Build a website or make changes with prompts.</p>
                 </div>
-            </div>
-        </section>
-
-
-
-<section className="mt-2">
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center py-12 md:pb-20">
-            <h2 className="h2 mb-4">Really Easy To Use Website Builder</h2>
-            <p className="text-xl text-gray-600">Notion Bear is a powerful website builder for startups, solo-entrepreneurs and hackers. Try it for free.</p>
-          </div>
-
-
-
-          {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
-
-            {/* 1st item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/aipowered.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">AI-Powered</h4>
-              <p className="text-gray-600 text-center">Build a website or make changes with prompts.</p>
               </div>
-            </div>
 
-            {/* 2nd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/moneypaid.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Accept Payments</h4>
-              <p className="text-gray-600 text-center">Accept payments with secure Stripe</p>
+              {/* 2nd item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/moneypaid.png"
+                />
+                <div>
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Accept Payments</h4>
+                  <p className="text-gray-600 text-center">Accept payments with secure Stripe</p>
+                </div>
               </div>
-            </div>
 
-            {/* 3rd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/handsup.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">100% No Code</h4>
-              <p className="text-gray-600 text-center">No need to code to build a powerful website.</p>
+              {/* 3rd item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/handsup.png"
+                />
+                <div>
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">100% No Code</h4>
+                  <p className="text-gray-600 text-center">No need to code to build a powerful website.</p>
+                </div>
               </div>
-            </div>
 
-            {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/uicomponents.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">UI & UX-Ready</h4>
-              <p className="text-gray-600 text-center">No need to be a designer.</p>
+              {/* 4th item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/uicomponents.png"
+                />
+                <div>
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">UI & UX-Ready</h4>
+                  <p className="text-gray-600 text-center">No need to be a designer.</p>
+                </div>
               </div>
-            </div>
 
-            {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/customcode.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Custom Code</h4>
-              <p className="text-gray-600 text-center">If you need more, you can add custom code.</p>
+              {/* 5th item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/customcode.png"
+                />
+                <div>
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Custom Code</h4>
+                  <p className="text-gray-600 text-center">If you need more, you can add custom code.</p>
+                </div>
               </div>
-            </div>
 
-            {/* 6th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-            <img
-                        alt="image"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-full"
-                        src="../images/analytics.png"
-                    />
-              <div>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Full Analytics</h4>
-              <p className="text-gray-600 text-center">Know how your website performs.</p>
+              {/* 6th item */}
+              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                <img
+                  alt="image"
+                  loading="lazy"
+                  decoding="async"
+                  className="w-20 mb-2"
+                  src="../images/analytics.png"
+                />
+                <div>
+                  <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Full Analytics</h4>
+                  <p className="text-gray-600 text-center">Know how your website performs.</p>
+                </div>
               </div>
-            </div>
 
-          </div>
-</section>
+            </div>
+          </section>
 
 
 
@@ -186,8 +186,8 @@ export default function FeaturesBlocks() {
         </div>
       </div>
 
-      <br/>
-      <br/>
+      <br />
+      <br />
     </section>
   )
 }
