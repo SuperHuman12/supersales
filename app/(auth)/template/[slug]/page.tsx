5fa76098b30d0df4b38526adf5762101669cc4fb
usman724 <usmanpervaiz724@gmail.com>
2024-03-18 22:10:19 +0000

    fix

--- app/(auth)/template/[slug]/page.tsx
+++ app/(auth)/template/[slug]/page.tsx
@@ -1,11 +1,68 @@
-import React from 'react';
-import {_loadFromJson, _transformDataToPostPageView, renderContent} from '../../../utils/helper';
+"use client";
+import React, { useEffect, useState } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation'; // Adjusted imports for Next.js 13
+import { _loadFromJson, _transformDataToPostPageView, renderContent } from '../../../utils/helper';
 
-const Page = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
-  const content = await _loadFromJson();
-  const filterBySlug = content.filter((item: any) => item.id === slug)[0];
-  const _postPageView =  _transformDataToPostPageView(filterBySlug);
+interface CallToCopy {
+  text: string;
+  link: string;
+}
+
+interface ViewDemo {
+  text: string;
+  link: string;
+}
+
+interface Product {
+  logo: string;
+  name: string;
+  provider: string;
+  description: string;
+  callToCopy: CallToCopy;
+  ViewDemo: ViewDemo;
+}
+
+interface ContentSection {
+  content: string;
+}
+
+interface Proof {
+  screenshot: string;
+  youtubevideo: string;
+}
+
+interface FilterBySlugType {
+  id: string;
+  product: Product;
+  overview: ContentSection;
+  howItWorks: ContentSection;
+  configuration: ContentSection;
+  proof: Proof;
+}
+
+
+const Page: React.FC = () => {
+  const [filterBySlug, setFilterBySlug] = useState<FilterBySlugType | null>(null);
+  const [postPageView, setPostPageView] = useState<any[]>([]);
+  const pathname = usePathname();
+  const [searchParams]: any = useSearchParams();
+
+  useEffect(() => {
+    const slug = pathname.split('/').pop();
+
+    if (slug) {
+      const fetchData = async () => {
+        const content = await _loadFromJson(false);
+        const filteredContent = content.find((item: { id: string }) => item.id === slug) as FilterBySlugType;
+        if (filteredContent) {
+          setFilterBySlug(filteredContent);
+          const transformedData = _transformDataToPostPageView(filteredContent);
+          setPostPageView(transformedData);
+        }
+      };
+      fetchData();
+    }
+  }, [pathname, searchParams]); // Reacting to changes in pathname and searchParams
 
 
   if (!filterBySlug) {
@@ -28,7 +85,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
           <div className="text-gray-800">
             <p>{filterBySlug?.product?.description}</p>
           </div>
-          {/* <div className="flex">
+          <div className="flex">
           <div className="flex mr-1 space-x-2 items-center">
             <a href={filterBySlug?.product?.callToCopy?.link} className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
               <span>{filterBySlug?.product?.callToCopy?.text}</span>
@@ -39,12 +96,12 @@ const Page = async ({ params }: { params: { slug: string } }) => {
               <span>{filterBySlug?.product?.ViewDemo?.text}</span>
             </a>
           </div>
-          </div> */}
+          </div>
         </div>
       </div>
 
       <div>
-        {_postPageView.map((item: any, index: number) => renderContent(item, index))}
+        {postPageView.map((item: any, index: number) => renderContent(item, index))}
       </div>
     </div>
   );
