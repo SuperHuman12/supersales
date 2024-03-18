bed65dd92097a6f73ebdf2336143da98d85c7ee2
usman724 <usmanpervaiz724@gmail.com>
2024-03-18 21:46:25 +0000

    fix

--- app/(auth)/integration/[slug]/page.tsx
+++ app/(auth)/integration/[slug]/page.tsx
@@ -69,27 +69,27 @@ const Page: React.FC = () => {
         <div className="space-y-4">
           <div className="flex items-center space-x-4">
             <div className="w-20 h-20 rounded-full overflow-hidden">
-              <img src={filterBySlug.product.logo} alt="" className="w-full h-full object-contain" />
+              <img src={filterBySlug?.product?.logo} alt="" className="w-full h-full object-contain" />
             </div>
             <div className="space-y-2">
-              <h1 className="text-2xl font-bold text-gray-800">{filterBySlug.product.name}</h1>
-              <p className="text-md text-gray-600">{filterBySlug.product.provider}</p>
+              <h1 className="text-2xl font-bold text-gray-800">{filterBySlug?.product?.name}</h1>
+              <p className="text-md text-gray-600">{filterBySlug?.product?.provider}</p>
             </div>
           </div>
           <div className="text-gray-800">
-            <p>{filterBySlug.product.description}</p>
+            <p>{filterBySlug?.product?.description}</p>
           </div>
           <div className="flex">
             <div className="flex mr-1 space-x-2 items-center">
-              <a href={filterBySlug.product.callToAction.link} className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
-                <span>{filterBySlug.product.callToAction.text}</span>
+              <a href={filterBySlug?.product?.callToAction.link} className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
+                <span>{filterBySlug?.product?.callToAction.text}</span>
               </a>
             </div>
           </div>
         </div>
       </div>
       <div>
-        {postPageView.map((item, index) => renderContent(item, index))}
+        {postPageView?.map((item, index) => renderContent(item, index))}
       </div>
     </div>
   );
