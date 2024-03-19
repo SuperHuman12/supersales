9d00ca96477a8b3de3827260bca6e9b4d037bb64
Vaibhav <qvmail83@gmail.com>
2024-03-19 13:46:38 +0000

    trynow

--- app/(auth)/compare/page.tsx
+++ app/(auth)/compare/page.tsx
@@ -204,67 +204,84 @@ export default function ResetPassword() {
 
 
           <section>
-            <div className="flex space-y-6" >
-              <div className="space-y-4">
-                <div className="flex items-center space-x-4" >
-                  <div className="space-y-2" >
-                    <h1 className="text-2xl font-bold text-gray-800">Customize your task management workflow to suit your needs</h1>
-                    <p className="text-md text-gray-600">ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.</p>
+
+          <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
+              <div className="max-w-sm">
+                  <div className="flex items-center space-x-4">
+                      <div className="space-y-2">
+                          <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                          <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
+                              ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.
+                          </p>
+                      </div>
                   </div>
-                </div>
-                <div className="flex space-x-2 items-center" >
-                  <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
-                    <span>Sign Up</span>
-                    {/* SVG or icon for arrow here if needed */}
-                  </a>
-                </div>
-              </div>
-              <div className="relative w-full" >
-                <img src="../images/remotejobs.png" alt="" className="w-full h-full object-contain" />
-              </div>
-            </div>
-
-
-            <div className="flex space-y-6" >
-            <div className="relative w-full" >
-                <img src="../images/remotejobs.png" alt="" className="w-full h-full object-contain" />
-              </div>
-              <div className="space-y-4">
-                <div className="flex items-center space-x-4" >
-                  <div className="space-y-2" >
-                    <h1 className="text-2xl font-bold text-gray-800">Customize your task management workflow to suit your needs</h1>
-                    <p className="text-md text-gray-600">ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.</p>
+                  <div className="mt-6 space-y-3 text-base font-normal text-gray-700 lg:text-lg">
+                      <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
                   </div>
-                </div>
-                <div className="flex space-x-2 items-center" >
-                  <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
-                    <span>Sign Up</span>
-                    {/* SVG or icon for arrow here if needed */}
-                  </a>
-                </div>
               </div>
-            </div>
+              <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+          </div>
 
 
-            <div className="flex space-y-6" >
-            <div className="space-y-4">
-                <div className="flex items-center space-x-4" >
-                  <div className="space-y-2" >
-                    <h1 className="text-2xl font-bold text-gray-800">Customize your task management workflow to suit your needs</h1>
-                    <p className="text-md text-gray-600">ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.</p>
+
+          <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
+          <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+
+              <div className="max-w-sm">
+                  <div className="flex items-center space-x-4">
+                      <div className="space-y-2">
+                          <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                          <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
+                              ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.
+                          </p>
+                      </div>
                   </div>
-                </div>
-                <div className="flex space-x-2 items-center" >
-                  <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank">
-                    <span>Sign Up</span>
-                    {/* SVG or icon for arrow here if needed */}
-                  </a>
-                </div>
+                  <div className="mt-6 space-y-3 text-base font-normal text-gray-700 lg:text-lg">
+                      <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
+                  </div>
               </div>
-              <div className="relative w-full" >
-                <img src="../images/remotejobs.png" alt="" className="w-full h-full object-contain" />
+          </div>
+
+
+
+          <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
+              <div className="max-w-sm">
+                  <div className="flex items-center space-x-4">
+                      <div className="space-y-2">
+                          <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                          <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
+                              ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.
+                          </p>
+                      </div>
+                  </div>
+                  <div className="mt-6 space-y-3 text-base font-normal text-gray-700 lg:text-lg">
+                      <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
+                  </div>
               </div>
-            </div>
+              <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+          </div>
+
+
+          <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
+
+          <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+
+              <div className="max-w-sm">
+                  <div className="flex items-center space-x-4">
+                      <div className="space-y-2">
+                          <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                          <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
+                              ClickUp is what you use if you only need to monitor task progress. To avoid paying for ClickUp and be empowered to create something uniquely suited to you, try Notion for free.
+                          </p>
+                      </div>
+                  </div>
+                  <div className="mt-6 space-y-3 text-base font-normal text-gray-700 lg:text-lg">
+                      <a href="https://app.gitbook.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
+                  </div>
+              </div>
+          </div>
+
+
 
 
 
