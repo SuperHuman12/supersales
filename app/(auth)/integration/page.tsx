eb27fd5ef5f7ea5154086ec1fc2f5f28c6356eba
Vaibhav <qvmail83@gmail.com>
2024-03-24 15:32:32 +0000

    trynow

--- app/(auth)/integration/page.tsx
+++ app/(auth)/integration/page.tsx
@@ -19,43 +19,42 @@ const Integration = async () => {
 
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate mt-6">
               <div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Missing Integration? Ask for Help</a></div>
-          </div>
+            </div>
 
           </div>
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
-            {integrations.map((integration:any, index:number) => (
-              <Link key={index} href={"/integration/"+integration.id} >
+            {integrations.map((integration: any, index: number) => (
+              <Link key={index} href={"/integration/" + integration.id} >
                 <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-                    <div className='w-full'>
-                      <div className="flex items-center space-x-2 mb-4">
-                        <img className="w-10 h-10" src={integration?.product?.logo} alt={integration?.product?.name} />
-                        <span className="text-dark font-bold">{integration?.product?.name}  </span>
-                      </div>
-                      <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
-                        {integration?.callToCopy?.text || "Integrate"}
-                      </button>
+                  <div className='w-full'>
+                    <div className="flex items-center space-x-2 mb-4">
+                      <img className="w-10 h-10" src={integration?.product?.logo} alt={integration?.product?.name} />
+                      <span className="text-dark font-bold">{integration?.product?.name}  </span>
                     </div>
+                    <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
+                      {integration?.callToCopy?.text || "Integrate"}
+                    </button>
+                  </div>
                 </div>
               </Link>
             ))}
-            
-              <Link key=" " href="#0" >
-                <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-                    <div className='w-full'>
-                      <div className="flex items-center space-x-2 mb-4">
-                        <img className="w-10 h-10" src="https://" alt="---" />
-                        <span className="text-dark font-bold">and a lot more</span>
-                      </div>
-                      <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
-                        Add More
-                      </button>
+
+            <Link href="https://iframely.com/domains" target='_blank'>
+            <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border border-gray-600 border-dashed">
+                <div className="w-full">
+                    <div className="flex items-center space-x-2 mb-4">
+                        <div className="w-10 h-10 bg-gray-200 rounded-full" alt="---"></div>
+                        <span className="text-dark font-bold">And a lot more..</span>
                     </div>
+                    <button className="border border-gray-200 bg-orange-600 rounded w-full p-2 mt-4">Add More...</button>
                 </div>
-              </Link>
+            </div>
+
+            </Link>
 
           </div>
         </div>
-        
+
       </div>
     </section>
   );
