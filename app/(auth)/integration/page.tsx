3a7a72597948430a28314c12e702d9626ed04420
Vaibhav <qvmail83@gmail.com>
2024-03-24 15:19:24 +0000

    trynow

--- app/(auth)/integration/page.tsx
+++ app/(auth)/integration/page.tsx
@@ -25,19 +25,34 @@ const Integration = async () => {
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
             {integrations.map((integration:any, index:number) => (
               <Link key={index} href={"/integration/"+integration.id} >
-              <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
-                  <div className='w-full'>
-                    <div className="flex items-center space-x-2 mb-4">
-                      <img className="w-10 h-10" src={integration?.product?.logo} alt={integration?.product?.name} />
-                      <span className="text-dark font-bold">{integration?.product?.name}  </span>
+                <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                    <div className='w-full'>
+                      <div className="flex items-center space-x-2 mb-4">
+                        <img className="w-10 h-10" src={integration?.product?.logo} alt={integration?.product?.name} />
+                        <span className="text-dark font-bold">{integration?.product?.name}  </span>
+                      </div>
+                      <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
+                        {integration?.callToCopy?.text || "Integrate"}
+                      </button>
                     </div>
-                    <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
-                      {integration?.callToCopy?.text || "Integrate"}
-                    </button>
-                  </div>
                 </div>
               </Link>
             ))}
+            
+              <Link key=" " href="#0" >
+                <div className="cursor-pointer relative flex flex-col items-center p-6 bg-white rounded shadow-xl text-center h-full border">
+                    <div className='w-full'>
+                      <div className="flex items-center space-x-2 mb-4">
+                        <img className="w-10 h-10" src="https://" alt="---" />
+                        <span className="text-dark font-bold">and a lot more</span>
+                      </div>
+                      <button className="text-white bg-gray-900 rounded w-full p-2 mt-4">
+                        Add More
+                      </button>
+                    </div>
+                </div>
+              </Link>
+
           </div>
         </div>
         
