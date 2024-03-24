e22d114eb0db5c271a6e8c1cdcc9f4cb0d3e0f91
Vaibhav <qvmail83@gmail.com>
2024-03-24 00:47:36 +0000

    trynow

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -25,7 +25,7 @@ export default function FeaturesBlocks() {
             <div className="text-start mt-6 w-1/2">
               <h2 className="h2 font-bold mb-4">Your content lives in Notion</h2>
               <p className="text-gray-700">Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
 
             </div>
             <div className="flex justify-start mt-6 w-full bg-gray-200 p-8 rounded">
@@ -59,7 +59,7 @@ export default function FeaturesBlocks() {
             <div className="text-start mt-6 w-1/2">
               <h2 className="h2 font-bold mb-4">Customize to your brand</h2>
               <p className="text-gray-700">Style the look and feel of your site with no-code themes and designer templates. Everything can be customized inside NotionBear without code to make you proud of the unique site you share with the world. Add custom-code only if you want to.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
 
             </div>
             <div className="flex justify-start mt-6 w-full bg-gray-200 p-8 rounded">
@@ -69,7 +69,7 @@ export default function FeaturesBlocks() {
                   loading="lazy"
                   decoding="async"
                   className="w-full shadow-md border border-2 rounded border-gray-600"
-                  src="https://assets.super.so/186f4256-f467-4f79-8d6f-40defe12b9b4/images/33c22cfa-3a77-425c-80cd-48dba4b899d0/fast.svg"
+                  src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d41acd65-b5b1-4482-8444-5d811bf9bbd1/notion/w=4416,quality=90"
                 />
               </div>
             </div>
@@ -93,7 +93,7 @@ export default function FeaturesBlocks() {
             <div className="text-start mt-6 w-1/2">
               <h2 className="h2 font-bold mb-4">World-class Performance</h2>
               <p className="text-gray-700">Pages load instantly anywhere in the world giving your site visitors a pleasant and snappy experience—they’ll never close the page for taking too long to load. On average NotionBear sites perform better than any industry leading website builder.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
             </div>
             <div className="flex justify-start mt-6 w-full bg-gray-200 p-8 rounded">
               <div className="p-8">
@@ -127,7 +127,7 @@ export default function FeaturesBlocks() {
             <div className="text-start mt-6 w-1/2">
               <h2 className="h2 font-bold mb-4">Optimized for SEO</h2>
               <p className="text-gray-700">Social sharing cards are automatically created from page content and your site’s HTML is optimized to use best practices for search engines. NotionBear sites are automatically built with the ability to rank high in search and look great when shared on social media.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
             </div>
             <div className="flex justify-start mt-6 w-full bg-gray-200 p-8 rounded">
               <div className="p-8">
@@ -150,7 +150,6 @@ export default function FeaturesBlocks() {
             <div className="max-w-3xl mx-auto text-center py-12 md:pb-20">
               <h2 className="h2 mb-4">Really Easy To Use Website Builder</h2>
               <p className="text-xl text-gray-600">Notion Bear is a powerful website builder for startups, solo-entrepreneurs and hackers. Try it for free.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300"><div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div></div>
             </div>
 
 
@@ -250,9 +249,11 @@ export default function FeaturesBlocks() {
 
             </div>
 
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="300">
-              <div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">View all features</a></div>
-            </div>
+            <div className="max-w-xs mx-auto sm:max-w-none sm:flex aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300">
+    <div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Get free lifetime plan</a></div>
+    <div><a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">View all features</a></div>
+</div>
+
 
           </section>
 
