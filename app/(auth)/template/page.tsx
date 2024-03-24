0670dde1f738af8701eb641b97da114cdef47138
Vaibhav <qvmail83@gmail.com>
2024-03-24 09:49:11 +0000

    trynow

--- app/(auth)/template/page.tsx
+++ app/(auth)/template/page.tsx
@@ -25,10 +25,17 @@ export default async function Template() {
             <h1 className="h1 mb-4">Find your Template</h1>
             <p className="text-xl text-gray-600">Jumpstart your SaaS business with pre-built solutions from NotionBear and our community.</p>
           </div>
+
+          <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center aos-init aos-animate mt-6" data-aos="zoom-y-out" data-aos-delay="300">
+              <div><a className="btn text-dark bg-orange-600 hover:bg-orange-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://app.notionbear.com">Ask our Designer for Help</a></div>
+              <div><a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/template">View all Templates</a></div>
+          </div>
+
+
         
-          <div className="max-w-3xl mx-auto mt-20 aos-init aos-animate max-w-sm grid gap-8 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none ">
+          <div className="max-w-3xl mx-auto mt-20 aos-init aos-animate max-w-sm grid md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
           {templates.map((template:any, index:number) => (
-              <Link key={index} href={"/template/"+template.id}  className="cursor-pointer   m-2 relative flex items-center p-6 bg-white rounded shadow-xl h-full border">
+              <Link key={index} href={"/template/"+template.id}  className="cursor-pointer   m-2 relative flex items-center p-6 bg-white rounded shadow-xl h-full border mt-6">
                 <div>
                   <div>
                     <div className="items-start">
