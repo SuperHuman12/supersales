28b66df671090b4fb645332e39c91a85c0cf9823
Vaibhav <qvmail83@gmail.com>
2024-03-24 15:38:50 +0000

    trynow

--- app/(auth)/pricing/page.tsx
+++ app/(auth)/pricing/page.tsx
@@ -91,7 +91,7 @@ export default function Pricing() {
                                                         <div className="relative flex items-center">
                                                             <div className="flex h-5 items-center"><input type="checkbox" id="addon" checked name="addon" className="h-5 w-5 rounded border-gray-300 text-gray-900 focus:ring-gray-900" aria-label="Sub-directory blogs addon" /></div>
                                                             <div className="ml-3 text-base">
-                                                                <label className="font-normal text-gray-900"><span className="mr-3 inline-flex items-center rounded-md bg-gray-900 px-2.5 py-0.5 text-sm font-medium uppercase text-white">Free Design Services</span><span>limited time only.</span></label>
+                                                                <label className="font-normal text-gray-900"><span>Including </span><span className="mr-3 inline-flex items-center rounded-md bg-gray-900 px-2.5 py-0.5 text-sm font-medium uppercase text-white">Free Design Services</span><span>limited time only.</span></label>
                                                             </div>
 
                                                         </div>
@@ -215,7 +215,7 @@ export default function Pricing() {
                                                         <div className="relative flex items-center">
                                                             <div className="flex h-5 items-center"><input type="checkbox" id="addon" checked name="addon" className="h-5 w-5 rounded border-gray-300 text-gray-900 focus:ring-gray-900" aria-label="Sub-directory blogs addon" /></div>
                                                             <div className="ml-3 text-base">
-                                                                <label className="font-normal text-gray-900"><span className="mr-3 inline-flex items-center rounded-md bg-gray-900 px-2.5 py-0.5 text-sm font-medium uppercase text-white">Free Design Services</span><span>limited time only.</span></label>
+                                                            <label className="font-normal text-gray-900"><span>Including </span><span className="mr-3 inline-flex items-center rounded-md bg-gray-900 px-2.5 py-0.5 text-sm font-medium uppercase text-white">Free Design Services</span><span>limited time only.</span></label>
                                                             </div>
 
                                                         </div>
