c67774b839bb0772184749a73e7d8da6d508be36
Vaibhav <qvmail83@gmail.com>
2024-05-07 21:01:00 +0000

    trynow

--- app/(auth)/compare-against/TemplateDesign.tsx
+++ app/(auth)/compare-against/TemplateDesign.tsx
@@ -9,9 +9,10 @@ const TemplateDesign = () => {
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Webflow is what you use if you only need to monitor task progress. To avoid paying for Webflow and be empowered to create something uniquely suited to you, try NotionBear for free.
+                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+
                         </p>
                     </div>
                 </div>
@@ -19,20 +20,21 @@ const TemplateDesign = () => {
                     <a href="https://app.notionbear.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
                 </div>
             </div>
-            <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
         </div>
 
 
 
         <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
-        <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
 
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Webflow is what you use if you only need to monitor task progress. To avoid paying for Webflow and be empowered to create something uniquely suited to you, try NotionBear for free.
+                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+
                         </p>
                     </div>
                 </div>
@@ -48,9 +50,10 @@ const TemplateDesign = () => {
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Webflow is what you use if you only need to monitor task progress. To avoid paying for Webflow and be empowered to create something uniquely suited to you, try NotionBear for free.
+                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+
                         </p>
                     </div>
                 </div>
@@ -58,20 +61,21 @@ const TemplateDesign = () => {
                     <a href="https://app.notionbear.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
                 </div>
             </div>
-            <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
         </div>
 
 
         <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
 
-        <div className="relative w-full"><img src="../images/remotejobs.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
 
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize your task management workflow to suit your needs</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Webflow is what you use if you only need to monitor task progress. To avoid paying for Webflow and be empowered to create something uniquely suited to you, try NotionBear for free.
+                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+
                         </p>
                     </div>
                 </div>
