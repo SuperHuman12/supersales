0720685373aefcd413982e7b7b5dededef226476
Vaibhav <qvmail83@gmail.com>
2024-05-08 05:34:51 +0000

    trynow

--- app/(auth)/compare-against/TemplateDesign.tsx
+++ app/(auth)/compare-against/TemplateDesign.tsx
@@ -20,20 +20,20 @@ const TemplateDesign = () => {
                     <a href="https://app.notionbear.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
                 </div>
             </div>
-            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover" /></div>
         </div>
 
 
 
         <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
-        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/notion%20to%20website.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover" /></div>
 
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Customize to your brand</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+                        Style the look and feel of your site with no-code themes and designer templates. Everything can be customized inside NotionBear without code to make you proud of the unique site you share with the world. Add custom-code only if you want to.
 
                         </p>
                     </div>
@@ -50,9 +50,9 @@ const TemplateDesign = () => {
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">World-class Performance</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+                        Pages load instantly anywhere in the world giving your site visitors a pleasant and snappy experience—they’ll never close the page for taking too long to load. On average NotionBear sites perform better than any industry leading website builder.
 
                         </p>
                     </div>
@@ -61,20 +61,20 @@ const TemplateDesign = () => {
                     <a href="https://app.notionbear.com/integrations/arcade" className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" rel="noopener noreferrer" target="_blank"><span>Sign Up</span></a>
                 </div>
             </div>
-            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+            <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/performancewebsite.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover" /></div>
         </div>
 
 
         <div className="grid grid-cols-1 gap-y-8 gap-x-12 lg:grid-cols-2 lg:items-center xl:gap-x-16 mb-12">
 
-        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/write%20on%20notion.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover shadow-xl" /></div>
+        <div className="relative w-full"><img src="https://dazzling-cat.netlify.app/analyticsseo.png" alt="" className="h-full w-full rounded-2xl border border-gray-200 object-cover" /></div>
 
             <div className="max-w-sm">
                 <div className="flex items-center space-x-4">
                     <div className="space-y-2">
-                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">Your content lives in Notion</h1>
+                        <h1 className="mt-4 text-2xl font-bold text-gray-900 sm:text-3xl">In built Analytics</h1>
                         <p className="mt-2 text-base font-normal text-gray-700 lg:text-lg">
-                            Your work stays in a place you control and love, while NotionBear handles the technical parts of publishing it to your website. This means you can focus what’s actually important to you: creating content and building your brand without worrying about configurations, plugins, downtime, performance, or security.
+                        Social sharing cards are automatically created from page content and your site’s HTML is optimized to use best practices for search engines. NotionBear sites are automatically built with the ability to rank high in search and look great when shared on social media.
 
                         </p>
                     </div>
