e0567fa554c395deaf1acaf5c7e243313db88811
Vaibhav <qvmail83@gmail.com>
2024-05-12 15:08:23 +0000

    trynow

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -24,7 +24,6 @@ export default function Testimonials() {
         </svg>
       </div>
 
-
       <div className="px-6 pt-16 mb-10 md:pt-20 max-w-6xl mx-auto px-4 sm:px-6 pb-12">
         <h2 className="mb-1 font-semibold leading-6 tracking-wide text-center text-orange-600 uppercase">
           About supersalesagro
@@ -64,7 +63,6 @@ export default function Testimonials() {
           </div>
         </div>
       </div>
-
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
@@ -234,79 +232,58 @@ export default function Testimonials() {
         </div>
       </div>
 
-
-
       <div className="px-6 pt-16 mb-10 md:pt-20 max-w-6xl mx-auto px-4 sm:px-6 pb-12">
         <h2 className="mb-1 font-semibold leading-6 tracking-wide text-center text-orange-600 uppercase">
-          About supersalesagro
+          About Super Sales Agro
         </h2>
         <h3 className="pb-8 text-2xl font-black text-center text-gray-900 sm:text-4xl tracking-snug sm:leading-15 md:leading-20">
-          Ready to ship?  Come see how well we sell.  
+          Ready to ship?  Come see how well we sell.
         </h3>
         <div className="grid items-start pt-6 mx-auto gap-8 md:flex md:items-start justify-center">
+
           <div className="float-right w-1/2 mb-6 ml-6 sm:float-none sm:mb-0 ">
-
-
             <Image src={addresspart} unoptimized alt="Logo" width="600"
               height="905"
-              className="rounded-md"/>
-
-
+              className="rounded-md" />
           </div>
 
 
-          <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-      <div className="flex justify-between items-center px-6 py-4">
-        <button className="bg-yellow-300 text-white font-bold py-2 px-4 rounded hover:bg-yellow-400 flex items-center">
-          <svg className="w-4 h-4 mr-2 fill-current" viewBox="0 0 20 20"><path d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zM9 5h2v5H9V5zm0 6h2v2H9v-2z"/></svg>
-          Join Channel
-        </button>
-        <button className="bg-green-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 flex items-center">
-          <svg className="w-4 h-4 mr-2 fill-current" viewBox="0 0 20 20"><path d="M18 8h-2V5a2 2 0 0 0-2-2h-3V0H9v3H6a2 2 0 0 0-2 2v3H2a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h4v-2H3v-5h14v5h-3v2h4a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zM5 5a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3H5V5z"/></svg>
-          Message on Whatsapp
-        </button>
-      </div>
-      <div className="px-6 py-4">
-        <div className="flex items-center">
-          <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 21v-2a4 4 0 014-4h10a4 4 0 014 4v2M16 3.13a4 4 0 100 7.75 4 4 0 000-7.75zM8 3.13a4 4 0 100 7.75 4 4 0 000-7.75z" /></svg>
-          <p className="text-gray-600">A-285, 1st and 2nd floor, New Subzi Mandi, Azadpur, Delhi-110034</p>
-        </div>
-        <div className="flex items-center mt-4">
-          <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5h12M9 3v2m0 4v10m-6 4h12a2 2 0 002-2V7a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
-          <p className="text-gray-600">+91-9899262264</p>
-          <p className="text-gray-600 ml-4">+91-981645230</p>
-        </div>
-        <div className="flex items-center mt-4">
-          <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 2H8a2 2 0 00-2 2v14a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2zM8 6h8m-8 4h8m-8 4h4" /></svg>
-          <p className="text-gray-600">hello@reallygreatsite.com</p>
-        </div>
-      </div>
-    </div>
-
-
-
-            <p className='my-2'>
-              Hey 👋 <br className="block sm:hidden" />
-              I’m Vaibhav, the founder of supersalesagro.
-            </p>
-            <p className='my-2'>
-              There are already a couple of website builders and knowledge base softwares out there to help you post and manage content. And while most of them are certainly doing a good job,
-              <strong className="text-gray-900">are they actually making use of the best content management system in the world?</strong>
-            </p>
-            <p className='my-2'>
-              I have been using <strong className="text-gray-900">Notion</strong> for a few years now. It's an amazing tool for storing and organizing all of my company's knowledge and writings. It should feel similiar to tools such as Intercom or Zendesk when it comes to support or webflow and framer if your building catalogs with 1000s of documents. <br />
-              <br />
-              Since I couldn't find the right solution – <strong className="text-gray-900">I built supersalesagro</strong>. Sure, you can publish your Notion page on its own but it might come across as unprofessional. The messy Notion url, distracting links pointing to
-              Notion and the lack of customizability are just a few disadvantages...
-            </p>
-            <p className='my-2'><strong className="text-gray-900">supersalesagro is solving all of this. It is for businesses who need an easy way to create a reliable knowledge site powered by Notion. </strong></p>
-            <br /> <br />
-            <p className='my-2'>— Vaibhav, Founder</p>
+          <div className="w-1/2 mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-between items-center px-6 py-4">
+              <button className="bg-yellow-300 text-white font-bold py-2 px-4 rounded hover:bg-yellow-400 flex items-center">
+                <svg className="w-4 h-4 mr-2 fill-current" viewBox="0 0 20 20"><path d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zM9 5h2v5H9V5zm0 6h2v2H9v-2z" /></svg>
+                Join Channel
+              </button>
+              <button className="bg-green-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 flex items-center">
+                <svg className="w-4 h-4 mr-2 fill-current" viewBox="0 0 20 20"><path d="M18 8h-2V5a2 2 0 0 0-2-2h-3V0H9v3H6a2 2 0 0 0-2 2v3H2a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h4v-2H3v-5h14v5h-3v2h4a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zM5 5a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3H5V5z" /></svg>
+                Message on Whatsapp
+              </button>
+            </div>
+            <div className="px-6 py-4">
+              <div className="flex items-center">
+                <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 21v-2a4 4 0 014-4h10a4 4 0 014 4v2M16 3.13a4 4 0 100 7.75 4 4 0 000-7.75zM8 3.13a4 4 0 100 7.75 4 4 0 000-7.75z" /></svg>
+                <p className="text-gray-600">A-285, 1st and 2nd floor, New Subzi Mandi, Azadpur, Delhi-110034</p>
+              </div>
+              <div className="flex items-center mt-4">
+                <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5h12M9 3v2m0 4v10m-6 4h12a2 2 0 002-2V7a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
+                <p className="text-gray-600">+91-9899262264</p>
+                <p className="text-gray-600 ml-4">+91-981645230</p>
+              </div>
+              <div className="flex items-center mt-4">
+                <svg className="w-6 h-6 mr-2 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 2H8a2 2 0 00-2 2v14a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2zM8 6h8m-8 4h8m-8 4h4" /></svg>
+                <p className="text-gray-600">hello@reallygreatsite.com</p>
+              </div>
+            </div>
           </div>
+
+
         </div>
+      </div>
 
 
-      </section>
+    </section>
+
+
 
   )
 }
