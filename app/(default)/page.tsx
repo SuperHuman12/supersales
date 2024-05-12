4101a6bd4e77a6692264b938c5848a03b0642d3d
Vaibhav <qvmail83@gmail.com>
2024-05-12 13:44:37 +0000

    trynow

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -16,9 +16,8 @@ export default function Home() {
     <>
       <Hero />
       <FeaturesBlocks />
-      <Showcases />
-      <Testimonials />
       <Newsletter />
+      <Testimonials />
     </>
   )
 }
