dd8a739b306e270f74f9cf724366335c972719ce
Vaibhav <qvmail83@gmail.com>
2024-05-20 14:32:46 +0000

    trynow

--- app/(auth)/compare-against/TrustedBy.tsx
+++ app/(auth)/compare-against/TrustedBy.tsx
@@ -62,7 +62,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/3d6d8b9e-51b7-4733-8611-12babc56d8c3/Strapi/w=441.59999999999997,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/01/Zespri-250x200.png"
             />
         </div>
         {/* Image 2 */}
@@ -75,7 +75,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/1ea0aa17-ae1d-48f6-a613-2b9aa5ad3db3/gumroad-min_(1)/w=736,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/01/Manson-250x200.png"
             />
         </div>
         {/* Image 3 */}
@@ -88,7 +88,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/8f49a317-faff-4d7b-b378-a151042a1ce3/circle/w=294.4,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/11/Northern-Fruit-250x200.png"
             />
         </div>
         {/* Image 4 */}
@@ -101,7 +101,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/d4e13333-1551-4114-b1a6-d2a7041c58a4/spline/w=441.59999999999997,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/01/Devos-250x200.png"
             />
         </div>
         {/* Image 5 */}
@@ -114,7 +114,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/fe61ffd3-2569-4810-b59c-06ff0f58e32f/Clubhouse/w=441.59999999999997,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/01/Chelan-250x200.png"
             />
         </div>
         {/* Image 6 */}
@@ -127,7 +127,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/4f2d2e91-850a-49df-b099-c685aba7c766/browser-company/w=441.59999999999997,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/01/Evelina-250x200.png"
             />
         </div>
         {/* Image 7 */}
@@ -140,7 +140,7 @@ export default function TrustedBy() {
                 decoding="async"
                 className="flex items-center justify-center py-2 col-span-2 md:col-auto m-auto w-1/2 md:w-full p-4"
                 unoptimized
-                src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/cba078a4-ecf1-4336-97d9-bffabc5fb432/Harmony/w=736,quality=90"
+                src="https://suriagrofresh.com/wp-content/uploads/2021/11/Natcha-250x200.png"
             />
         </div>
     </div>
