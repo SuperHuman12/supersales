273616567c29f8653d1ee54ecdc547a356d41060
Vaibhav <qvmail83@gmail.com>
2024-05-22 19:10:20 +0000

    trynow

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -56,9 +56,7 @@ export default function Testimonials() {
   <p className='my-2'>
     Super Sales Agro is one of the pioneering entities in the agricultural sector, committed to delivering <strong className="text-gray-900">the best rates</strong> with solutions that revolutionize farming practices. Specializing in the distribution and management of agricultural produce, ensuring that every step from the farm to the consumer is handled with <strong className="text-gray-900">utmost precision and care</strong>.
   </p>
-  <p className='my-2'>
-    We believe in creating experiences that resonate deeply with our producers and buyers. The satisfaction of our farmers working with us is just as important as the delight of our customers savoring our produce. We meticulously oversee the journey of our products, guaranteeing they reach consumers in <strong className="text-gray-900">the highest quality possible</strong>.
-  </p>
+
   <p className='my-2'>
     Founded with a vision to address the challenges in the agricultural industry, <strong className="text-gray-900">SSA DELHI</strong> has embarked on a journey to establish itself as a leader in the field. Over the years, we have expanded into various sectors including cold chain logistics, packaging, and advanced agricultural technology. Our use of <strong className="text-gray-900">state-of-the-art machinery</strong> and adherence to international standards has positioned us at the forefront of the global agricultural market.
   </p>
@@ -245,7 +243,7 @@ export default function Testimonials() {
               </button>
             </div>
             <div className="px-6 py-4 gap-4 grid">
-            <a href="https://twitter.com/absurdfounder" aria-label="Twitter">
+            <a href="https://twitter.com/absurdfounder" aria-label="Twitter" className="mb-2">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
                 </svg>
@@ -256,14 +254,14 @@ export default function Testimonials() {
                 <path d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0m-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
                 </svg>
                 <p className="text-gray-600">+91-9899262264</p>
-                <p className="text-gray-600 ml-4">+91-981645230</p>
+                <p className="text-gray-600 ml-4">+91-9711269346</p>
               </a>
               <a href="https://twitter.com/absurdfounder" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
                  <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
                 </svg>
-                <p className="text-gray-600">hello@reallygreatsite.com</p>
+                <p className="text-gray-600">supersalesagro@gmail.com</p>
               </a>
               <a href="https://twitter.com/absurdfounder" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
