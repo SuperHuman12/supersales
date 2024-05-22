31517562f9aef0043d6d12957dcf2beff565d6f6
Vaibhav <qvmail83@gmail.com>
2024-05-22 19:16:52 +0000

    trynow

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -63,9 +63,6 @@ export default function Testimonials() {
   <p className='my-2'>
     Our dedication to sustainable growth and prices ensures that we not only meet the demands of our farmers/suppliers but exceed the expectations of our buyers. At supersalesagro, we operate like a family, together cultivating a future where <strong className="text-gray-900">supply and quality go hand in hand</strong>.
   </p>
-  <p className='my-2'>
-    — Sushil Kalra
-  </p>
 </div>
 
 
@@ -249,13 +246,17 @@ export default function Testimonials() {
                 </svg>
                 <p className="text-gray-600">A-285, 1st and 2nd floor, New Subzi Mandi, Azadpur, Delhi-110034</p>
               </a>
-              <a href="https://twitter.com/absurdfounder" aria-label="Twitter" className="mb-4 border p-4 rounded-md hover:border-gray-600 focus:shadow-md">
+              <div className="mb-4 p-4 rounded-md">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0m-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
                 </svg>
+                <a href="https://twitter.com/absurdfounder" aria-label="Twitter" className="mb-4 border p-4 rounded-md hover:border-gray-600 focus:shadow-md">
                 <p className="text-gray-600 ml-4">+91-9899262264</p>
+                </a>
+                <a href="https://twitter.com/absurdfounder" aria-label="Twitter" className="mb-4 border p-4 rounded-md hover:border-gray-600 focus:shadow-md">
                 <p className="text-gray-600 ml-4">+91-9711269346</p>
-              </a>
+                </a>
+              </div>
               <a href="https://twitter.com/absurdfounder" aria-label="Twitter" className="mb-4 border p-4 rounded-md hover:border-gray-600 focus:shadow-md">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
